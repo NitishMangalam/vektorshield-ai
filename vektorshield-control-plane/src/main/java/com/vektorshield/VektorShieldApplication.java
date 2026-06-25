76a9f381bc13836fe2b32b1aa2d0a94dfc636d33
NitishMangalam <mangalamnitish@gmail.com>
2026-06-25 14:51:28 +0000

    feat: implement primary VektorShieldApplication main runner class

---
+++ vektorshield-control-plane/src/main/java/com/vektorshield/VektorShieldApplication.java
@@ -0,0 +1,13 @@
+package com.vektorshield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@EnableAspectJAutoProxy
+public class VektorShieldApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VektorShieldApplication.class, args);
+    }
+}
